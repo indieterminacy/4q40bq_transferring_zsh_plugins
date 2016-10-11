726a19dd314e1750593623dc1015a5207348ec1c
indieterminacy <indieterminacy@nortonspeal.com>
2016-10-11 11:51:47 +0200

    Starting repo. Started repo for Zsh plugins, covering transferring

---
+++ oqo_transferring.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/zsh
+
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/rsync
+#https://github.com/jsks/zsh-plugins/tree/master/zerve
+#https://github.com/jsks/czhttpd
+#http://zshwiki.org/home/code/scripts/zshttpd
+#https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/iwhois/iwhois.plugin.zsh
+#
+####################
+# oq SSH
+####################
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/ssh-agent
+####################
+# oq Fabric
+####################
+# Fabric autocompletion for ZSH
+#https://github.com/vhbit/fabric-zsh-autocomplete
+####################
+# bq downloading
+####################
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/httpie
