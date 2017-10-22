4cb8ce4ea1bd66fd90887e16fc2199861e6e1095
indieterminacy <indieterminacy@nortonspeal.com>
2017-10-22 21:35:28 +0200

    Added annotations

--- oqo_transferring.zsh
+++ oqo_transferring.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/zsh
 
 #https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/rsync
+# Share files from the command line
 #https://github.com/jsks/zsh-plugins/tree/master/zerve
+# ZSH Http Server
 #https://github.com/jsks/czhttpd
 #http://zshwiki.org/home/code/scripts/zshttpd
 #https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/iwhois/iwhois.plugin.zsh
